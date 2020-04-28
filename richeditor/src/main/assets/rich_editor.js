1866e870504351d9b846b3836de7bb49ff1bcff4
Mary Liliana Falcon Lopez <mfl@inovante.mx>
2020-04-27 20:08:37 -0500

    Prueba de test de texto seleccionado

--- richeditor/src/main/assets/rich_editor.js
+++ richeditor/src/main/assets/rich_editor.js
@@ -158,6 +158,19 @@ RE.setTextBackgroundColor = function(color, uuid) {
 
 RE.saveTags = function(uuid){
     var array = document.querySelectorAll('strong[style^="background-color: rgb"], span[style^="background-color: rgb"]');
+    var att = document.createAttribute("data-id");
+    att.value = uuid;
+    for (var i=0; i<array.length; i++)
+    {
+        var atributo = array[i].getAttribute("data-id");
+        if (atributo === null) {
+            array[i].setAttribute(att);
+        }
+    }
+}
+
+RE.saveTags2 = function(uuid){
+    var array = document.querySelectorAll('strong[style^="background-color: rgb"], span[style^="background-color: rgb"]');
     alert(array.length);
     for (var i=0; i<array.length; i++)
     {
