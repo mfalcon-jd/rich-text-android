2377a64f54e6dbeb8a89e01a1c222f05321936db
Mary Liliana Falcon Lopez <mfl@inovante.mx>
2020-05-13 19:24:37 -0500

    Adecuaciones para seleccionado de texto validar

--- richeditor/src/main/assets/rich_editor.js
+++ richeditor/src/main/assets/rich_editor.js
@@ -155,7 +155,7 @@ RE.setTextBackgroundColor = function(color, uuid) {
     document.execCommand("styleWithCSS", null, false);
     document.execCommand("copy", null, null);
     RE.saveTags(uuid);
-    alert(RE.editor.outerHTML);
+    console.log(RE.editor.outerHTML);
 }
 
 RE.saveTags = function(uuid){
